25c11016d8e2a0fda777d24811e562f5b7e4a2c0
Suhas <suhas.chamarty@gmail.com>
2025-09-20 11:28:28 +0530

    sum

---
+++ 10-shell.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MGS_CHARS=("Solid snake", "Liquid Snake", "Big Boss", "The Boss")
+
+echo "All characters of MGS are:" ${MGS_CHARS[@]}
+
+echo "The Main Root Character of MGS is:" ${MGS_CHARS[3]}
