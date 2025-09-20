cdb2d73ed2cd52221a82a98b62c40138380b82cf
Suhas <suhas.chamarty@gmail.com>
2025-09-20 01:00:59 -0400

    One more

--- 07-shell.sh
+++ 07-shell.sh
@@ -2,4 +2,13 @@
 
 d=$(date)
 
-echo "Executed date: $d"
+start_time=$(date + %s)
+
+sleep 10 &
+
+end_time=$(date + %s)
+
+tt=$(($end_time-$start_time))
+echo "Execution start:" $start_time
+echo "Execution ended:" $end_time
+echo "time taken for exectuion
