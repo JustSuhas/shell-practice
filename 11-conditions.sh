8e62fab69135d0616467e7a3add0c34a32487932
Suhas <suhas.chamarty@gmail.com>
2025-09-22 20:16:57 +0530

    Shell-prac

---
+++ 11-conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUM1=$1
+
+if [ $NUM1 -lt 10]; then
+    echo "Given no: is less than 10"
+
+elif [ $NUM1 -eq 10]; then
+    echo "Given no: is 10"
+else
+    echo "Given no: is greather than 10"
+fi
